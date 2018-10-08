ec66b73bea36a26066870b0afa4685c543f14849
Alexander Tokos <alext_1995@hotmail.com>
2018-10-08 13:55:27 +0300

    Rewrite led class with a update function that blinks the leds.

--- lab-4/led.c
+++ lab-4/led.c
@@ -2,32 +2,14 @@
 #include "ports.h"
 
 #include "led.h"
-#include "timer.h"
 
 /* DELAY in ms */
 void Led_Init() {
-  /* Turn off both leds */
-  Led_Set(RED, OFF);
-  Led_Set(GREEN, OFF);
+  LED_PORT_DIRECTION |= RED_LED_PIN + GREEN_LED_PIN;
+  LED_PORT |= RED_LED_PIN;
+  LED_PORT &= ~GREEN_LED_PIN;
 }
 
-void Led_Set(tByte LED, tByte STATE) {
-  switch(LED) {
-    case RED:
-      if(STATE == ON){
-        LED_PORT_DIRECTION |= RED_LED_PIN;
-        LED_PORT |= RED_LED_PIN;
-      }
-      else{
-        LED_PORT_DIRECTION &= ~(RED_LED_PIN);
-      }
-    case GREEN:
-    if(STATE == ON){
-        LED_PORT_DIRECTION |= GREEN_LED_PIN;
-        LED_PORT |= GREEN_LED_PIN;
-      }
-      else{
-        LED_PORT_DIRECTION &= ~(GREEN_LED_PIN);
-      }
-  }
+void Led_Update() {
+  LED_PORT ^= RED_LED_PIN + GREEN_LED_PIN;
 }
